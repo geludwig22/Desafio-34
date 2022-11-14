1bb56d5cdd550a15a5ec29c0c556f8ca96fe63fc
geludwig22 <jorlupeve@hotmail.com>
2022-11-14 11:05:55 -0300

    First push

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,()=>console.log(`Listen on ${PORT} port`))
+
+app.get('/', (req,res)=>{
+    let info = {
+        message: "Vive!!!",
+        port:`Listen on ${PORT} port`
+    }
+    res.status(200).send(info)
+})
