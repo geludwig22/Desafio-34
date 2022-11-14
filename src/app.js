cec67b0804aed4eb801d4f25bb6e8a686edcfaed
geludwig22 <jorlupeve@hotmail.com>
2022-11-14 11:20:00 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT||8080
 
 app.listen(PORT,()=>console.log(`Listen on ${PORT} port`))
 
